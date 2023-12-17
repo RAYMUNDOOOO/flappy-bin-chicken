24b7249209b1344de88dd3b0ef665e9fdabeba5d
Ramon <ramontovar17@gmail.com>
2023-12-18 09:37:51 +1100

    Did nothing.

--- src/game.c
+++ src/game.c
@@ -3,7 +3,9 @@
 #include "pipe.h"
 #include "raylib.h"
 #include <stdlib.h>
+#include <stdio.h>
 
+int score = 0;
 int numPipes = 0;
 static Bird* bird = NULL;
 static Pipe* pipes = NULL;
@@ -25,6 +27,7 @@ void TickGame(const float DELTA_TIME)
 {
 	TickBird(bird, DELTA_TIME);
 	TickPipe(pipes, numPipes, DELTA_TIME);
+	QueryCollisions(bird, pipes);
 }
 
 void DrawGame()
@@ -43,8 +46,26 @@ void CleanGame()
 
 void QueryCollisions(const Bird* bird, const Pipe* pipes)
 {
+	if (numPipes > 0)
+	{
+		for (int i = 0; i < numPipes; i++)
+		{
+			if (CheckCollisionRecs(bird->body, pipes[i].upperBody)) printf("Collision detected with upper body.\n");
+			if (CheckCollisionRecs(bird->body, pipes[i].lowerBody)) printf("Collision detected with lower body.\n");
+			if (CheckCollisionRecs(bird->body, pipes[i].scoreBody)) 
+			{
+				AddScore(1);	
+			}
+		}
+	}
 }
 
 void AddScore(const int value)
 {
+	score += value;
+}
+
+int GetScore()
+{
+	return score;
 }
