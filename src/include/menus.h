ff4788f3019b26369cd1eddcf2ae309c0fdc5290
Ramon Tovar <rtovar.develops@gmail.com>
2024-01-18 19:25:54 +1100

    Added raygui and menus.h to draw menus for different game states

---
+++ src/include/menus.h
@@ -0,0 +1,10 @@
+static const int MENU_WIDTH = 200;
+static const int MENU_HEIGHT = 300;
+static const int BUTTON_WIDTH = 180;
+static const int BUTTON_HEIGHT = 100;
+static const int LABEL_WIDTH = 180;
+static const int LABEL_HEIGHT = 100;
+
+void DrawGameOverMenu();
+void DrawPauseMenu();
+void DrawStartScreen();
